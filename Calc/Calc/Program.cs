d47d1893409550edfd3eb9dccdb980e1463784f1
goodluck-yh <goodluck.yh2014@gmail.com>
2016-08-31 22:29:15 +1000

    finish parse function

--- Calc/Calc/Program.cs
+++ Calc/Calc/Program.cs
@@ -10,6 +10,77 @@ namespace Calc
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(args[0]);
+            List<String> list = new List<string>();
+            Program program = new Program();
+            bool result = program.parse(args[0], list);
+            if(result == false)
+            {
+                Console.WriteLine("Invalid Input");
+            }else
+            {
+                Console.WriteLine("Valid Input");
+            }
+            //Console.Read();
+        }
+
+        public bool parse(String arg, List<String> list)
+        {
+            bool isNumber = true;
+            for(int i = 0; i < arg.Length; i++)
+            {
+                if(isNumber == true)
+                {
+                    if(arg.ElementAt(i) == '+' || arg.ElementAt(i) == '-')
+                    {
+                        String temp = arg.ElementAt(i).ToString();
+                        i++;
+                        if(i == arg.Length)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            if(arg.ElementAt(i) >= '0' && arg.ElementAt(i) <= '9')
+                            {
+                                temp = temp + arg.ElementAt(i).ToString();
+                                list.Add(temp);
+                                isNumber = false;
+                            }else
+                            {
+                                return false;
+                            }
+                        }
+                    }else if (arg.ElementAt(i) >= '0' && arg.ElementAt(i) <= '9')
+                    {
+                        String temp = arg.ElementAt(i).ToString();
+                        list.Add(temp);
+                        isNumber = false;
+                    }else
+                    {
+                        return false;
+                    }
+                }else
+                {
+                    if(arg.ElementAt(i) == '+' || arg.ElementAt(i) == '-' || arg.ElementAt(i) == '*' || arg.ElementAt(i) == '/' || arg.ElementAt(i) == '%')
+                    {
+                        String temp = arg.ElementAt(i).ToString();
+                        list.Add(temp);
+                        isNumber = true;
+                    }else
+                    {
+                        return false;
+                    }
+                }
+            }
+            if(isNumber == true)
+            {
+                return false;
+            }else
+            {
+                return true;
+            }
+            
         }
     }
 }
